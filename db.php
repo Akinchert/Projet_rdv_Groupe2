2b952380bb7fb415c9d6d6c77660acaea1d99f4d
Akinchert <akinchert619@gmail.com>
2025-05-23 15:54:49 +0200

    premier commit

--- db.php
+++ db.php
@@ -1,4 +1,13 @@
 <?php
-$pdo = new PDO("mysql:host=localhost;dbname=appointments", "root", "");
+// $pdo = new PDO("mysql:host=localhost;dbname=appointments", "root", "");
+// $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
+<?php
+$host = getenv("mysql.railway.internal");
+$db   = getenv("appointments");
+$user = getenv("root");
+$pass = getenv("irzxqzYcmmRunLnYZkIQjhTSvWMmadIt");
+
+$pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
